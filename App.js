2ca9f3d961b2fe7aef7fd8d7d4f9d6b2156c1bac
lourencogtac <lourenco@gtacsolutions.com>
2020-05-04 20:07:22 -0300

    Statusbar has been fixed

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import 'react-native-get-random-values';
-import {createAppContainer} from 'react-navigation';
-import React, {useState} from 'react';
-import {YellowBox} from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import React, { useState } from 'react';
+import { YellowBox, View, SafeAreaView, StyleSheet } from 'react-native';
 import Routes from './src/routes';
 import createStoreProvider from './src/store/createStoreProvider';
-import {UserProvider} from './src/store/user';
-import {SymptomProvider} from './src/store/symptom';
+import { UserProvider } from './src/store/user';
+import { SymptomProvider } from './src/store/symptom';
 
 const AppContainer = createAppContainer(Routes);
 
@@ -16,8 +16,22 @@ const StoreProvider = createStoreProvider([UserProvider, SymptomProvider]);
 
 export default class App extends React.Component {
   render = () => (
-    <StoreProvider>
-      <AppContainer />
-    </StoreProvider>
+    <View style={styles.appContainer}>
+      <SafeAreaView style={styles.safeAreaContainer}>
+        <StoreProvider>
+          <AppContainer />
+        </StoreProvider>
+      </SafeAreaView>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    backgroundColor: '#828282'
+  },
+  safeAreaContainer: {
+    height: "100%"
+  }
+});
